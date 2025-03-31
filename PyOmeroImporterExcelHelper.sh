f08219ce556c56987f6e91604f08869d2e254e1f
Alex Rigano <alex.rigano@umassmed.edu>
2025-03-31 13:24:15 -0400

    fixed bug in sh

--- PyOmeroImporterExcelHelper.sh
+++ PyOmeroImporterExcelHelper.sh
@@ -3,24 +3,28 @@ curDir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 script="${curDir}/fetch_images.py"
 excelFilePath="$1"
 isDebug="$2"
-result= false
-
+result=false
 
 activateScript="${curDir}/.venv/bin/activate"
 
 if [[ ! -f $activateScript ]]; then
     activateScript="${curDir}/venv3/bin/activate"
 fi
-echo "Activating venv ${activateScript}"
+if [[$isDebug]]; then
+    echo "Activating venv ${activateScript}"
+fi
 source $activateScript
 
 if [[ -n "$excelFilePath" ]]; then
     eval "excelFilePath=$excelFilePath"
     if [[ -f "$excelFilePath" ]]; then
-        echo "Running python for ${excelFilePath}"
+        if [[$isDebug]]; then
+            echo "Running python for ${excelFilePath}"
+        fi
         python3 $script $excelFilePath
     fi
 fi
-
-echo "Deactivating venv"
+if [[$isDebug]]; then
+    echo "Deactivating venv"
+fi
 deactivate
